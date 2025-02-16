ade42180331194b4c02fd3a804d04505f5f94e91
ruvl <ru.vlad.13@gmail.com>
2025-02-16 15:55:04 +0300

    Wi-Fi connection through GyverPortal works! Added Timer class. Added wifi config and setup functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,36 @@
 #include <Arduino.h>
+#include <EEManager.h>
+#include <GyverPortal.h>
+// #include <PubSubClient.h>
+
+#include "wifi.h"
+
+struct LampData {
+    WiFiConfig wifi_config = wifi_config;
+} data;
+
+EEManager memory(data);
+GyverPortal portal;
+
+// WiFiClient espClient;
+// PubSubClient client(espClient);
 
 void setup() {
-  // Serial.begin(9600);         // запускаем сериал для отладки
-  pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600); // запускаем сериал для отладки
+
+    // Initialize memory
+    EEPROM.begin(memory.blockSize());
+    memory.begin(0, 0);
+
+    wifiInit(data.wifi_config, memory, portal);
+    wifiSetup();
+
+
+    // client.setServer(mqtt_server, mqtt_port);
+    // client.setCallback(callback);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+    memory.tick(); // проверяем обновление настроек
+    portal.tick(); // пинаем портал
 }
